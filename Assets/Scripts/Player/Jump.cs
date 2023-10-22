e17bd3f77c793f64857411d4803d56c658381223
cashara <cashara@bk.ru>
2023-10-22 16:45:12 +0500

    start working on UI

--- Assets/Scripts/Player/Jump.cs
+++ Assets/Scripts/Player/Jump.cs
@@ -9,6 +9,7 @@ public class Jump : MonoBehaviour
     private Rigidbody2D _rb;
     [SerializeField] private float _jumpSpeed = 4.0f;
     [SerializeField] private GroundChecker _groundChecker;
+    [SerializeField] private PlayerHealth _playerHealth;
 
     private void OnValidate()
     {
@@ -24,7 +25,7 @@ public class Jump : MonoBehaviour
     private void Update()
     {
         _isJump = Input.GetButtonDown("Jump");
-        if (_isJump && _groundChecker.IsGround())  
+        if (_isJump && _groundChecker.IsGround() && !_playerHealth.IsDead)  
         {
             _rb.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
         }
